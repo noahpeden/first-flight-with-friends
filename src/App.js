41b9221dcc9f360796d2da7194204d85ce0c8384
Noah Peden <noahpeden@gmail.com>
2017-12-07 22:05:33 -0700

    minor db in fb changes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import firebase from './firebase';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      newData: ''
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: null,
+			newData: ''
+		};
 
-    this.dataRef = firebase.database().ref();
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+		this.dataRef = firebase.database().ref();
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-  componentWillMount() {
-    this.dataRef.on('value', (snapshot) => {
-      this.setState({
-        data: snapshot.val()
-      });
-    });
-  }
+	componentDidMount() {
+		database.ref().on('value', snapshot => {
+			this.setState({
+				data: snapshot.val()
+			});
+		});
+	}
 
-  handleChange(event) {
-    this.setState({
-      newData: event.target.value
-    });
-  }
+	componentWillMount() {
+		this.dataRef.on('value', snapshot => {
+			this.setState({
+				data: snapshot.val()
+			});
+		});
+	}
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.dataRef.push(this.state.newData);
-    this.setState({ newData: '' });
-  }
+	handleChange(event) {
+		const newData = event.target.value;
+		this.setState({
+			newData: newData
+		});
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React and Firebase</h2>
-        </div>
-        <pre className="App-data">
-          { JSON.stringify(this.state.data, null, 2) }
-        </pre>
-        <form className="App-form" onSubmit={this.handleSubmit}>
-          <input value={this.state.newData} onChange={this.handleChange} />
-          <input type="submit" disabled={!this.state.newData} />
-        </form>
-      </div>
-    );
-  }
+	handleSubmit(event) {
+		event.preventDefault();
+		this.dataRef.push(this.state.newData);
+		this.setState({ newData: '' });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<h2>Welcome to React and Firebase</h2>
+				</div>
+				<pre className="App-data">
+					{JSON.stringify(this.state.data, null, 2)}
+				</pre>
+				<form className="App-form" onSubmit={this.handleSubmit}>
+					<input value={this.state.newData} onChange={this.handleChange} />
+					<input type="submit" disabled={!this.state.newData} />
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBmOcQx1f6_UTnqbPjWffMe2RMlq3ClEbw',
-  authDomain: 'first-flight-2c57b.firebaseapp.com',
-  databaseURL: 'https://first-flight-2c57b.firebaseio.com',
-  storageBucket: 'first-flight-2c57b.appspot.com',
-  messagingSenderId: '270361986787'
+	apiKey: 'AIzaSyB0YcTSbPboUrEh9KrAW3VJehkK_xC0t4k',
+	authDomain: 'first-flight-with-friend-c0baa.firebaseapp.com',
+	databaseURL: 'https://first-flight-with-friend-c0baa.firebaseio.com',
+	projectId: 'first-flight-with-friend-c0baa',
+	storageBucket: 'first-flight-with-friend-c0baa.appspot.com',
+	messagingSenderId: '261060051739'
 };
-
 firebase.initializeApp(config);
 
 export default firebase;
+export const database = firebase.database();
